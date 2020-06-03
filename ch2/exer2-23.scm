cf5b4232250a8802fa83901304a5685d03802619
mojiajun <mojiajunmail@gmail.com>
2020-06-04 00:46:57 +0800

    exer2-23 添加了一些其他的

--- ch2/exer2-23.scm
+++ ch2/exer2-23.scm
@@ -20,3 +20,29 @@
                   (newline) 
                   (display x))
           (list 57 321 88))
+; 返回
+;   57
+;   321
+;   88
+;   ;Unspecified return value, 这个是因为cond在最后没有满足的cond的值没有定义. 见教材1.1.6
+
+;
+;(define (abs-v2 x)
+;  (cond ((< x 0) (- x))
+;        ))
+;
+;(define val1 (abs-v2 -5))
+;val1
+;(define val2 (abs-v2 10))
+;val2
+;; Unspecified return value
+
+(define (abs-v3 x)
+  (if (< x 0) 
+      (- x)))
+
+(define val1 (abs-v3 -5))
+val1
+(define val2 (abs-v3 10))
+val2
+; 这个是Unspecified return value, 和cond一样
