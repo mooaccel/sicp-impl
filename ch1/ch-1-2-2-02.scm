70b980aa3665e6737095706c49bf28ddfedea4c2
mojiajun <mojiajunmail@gmail.com>
2020-06-03 20:48:24 +0800

    换零钱问题

---
+++ ch1/ch-1-2-2-02.scm
@@ -0,0 +1,23 @@
+; denomination: 面值
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+; 可以重复拿, 那就是完全背包问题了?
+(define (count-change amount)
+  (define (cc amount kinds-of-coins)
+    (cond ((= amount 0) 1)
+          ((or (< amount 0) (= kinds-of-coins 0)) 0)
+          (else (+ (cc amount
+                       (- kinds-of-coins 1))
+                   (cc (- amount 
+                          (first-denomination kinds-of-coins))
+                       kinds-of-coins)))))
+  (cc amount 5))
+
+(count-change 100)
+
+;; Counting change
