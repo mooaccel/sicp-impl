e31643b3d754debaab5a66136ffb8352562b2c07
mojiajun <mojiajunmail@gmail.com>
2020-06-03 14:09:21 +0800

    append func

--- ch2/ch-2-2-1-01.scm
+++ ch2/ch-2-2-1-01.scm
@@ -13,6 +13,7 @@ list1
       (+ 1 (length (cdr items)))))
 (length list1)
 
+; iterative process
 ; 下面这种实现是iterative style
 ; 是recursive procedure 但是因为scheme这种函数式编程语言都进行了tail recursive优化
 ; 其实本质是迭代的. 类似语法糖...
@@ -26,3 +27,15 @@ i; 区分的目的就是为了看明白length-iter这种实现...
         (length-iter-help (cdr seqs) (+ 1 count))))
   (length-iter-help items 0))
 (length-iter (list 1 100 3 3 4))
+
+; recursive process
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) 
+            (append (cdr list1) list2))))
+
+(define squaresVals (list 1 4 9 16 25))
+(define oddsVals (list 1 3 5 7))
+(append squaresVals oddsVals)
+(append oddsVals squaresVals)
