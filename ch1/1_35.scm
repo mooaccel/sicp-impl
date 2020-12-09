4424552fb88252ff70a29cacb7c059fc43fdc68f
mojiajun <mojiajunmail@gmail.com>
2020-12-10 00:02:26 +0800

    complement 1_35

--- ch1/1_35.scm
+++ ch1/1_35.scm
@@ -1,3 +1,5 @@
+; 1. fixed-point func
+; 2. sqrt func
 (load "./ch1/p92_fixed_point.scm")
 
 (define (golden-rate)
@@ -5,3 +7,14 @@
                               1))
                1.0))
 (golden-rate)
+
+; why golden ratio ϕ is fixed point of x -> 1 + 1/x
+; 1 + 1/x = x
+; x + 1 = x^2
+; x^2 - x - 1 = 0
+; a = 1, b = -1, c = -1
+; (1 +/- √5) / 2
+(/ (+ 1 (sqrt 5))
+   2.0)
+(/ (- 1 (sqrt 5))
+   2.0)
