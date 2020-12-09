de944490fe9de5511a5682a20d5261975453e525
mojiajun <mojiajunmail@gmail.com>
2020-12-09 20:55:24 +0800

    add p87_let_lambda_equivalence.scm

---
+++ ch1/p87_let_lambda_equivalence.scm
@@ -0,0 +1,13 @@
+; 等价!!! 
+; (define (f x y)
+;   (let ((a (+ 1 (* x y)))
+;         (b (- 1 y)))
+;   (+ (* x (square a)) (* y b) (* a b))))
+(define (f x y)
+  ((lambda (a b) 
+    (+ (* x (square a)) 
+       (* y b) 
+       (* a b)))
+  (+ 1 (* x y))
+  (- 1 y)))
+(f 10 20)
