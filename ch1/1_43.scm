2900ad8116616832aa1a5557136d12fb5b69bcb8
mojiajun <mojiajunmail@gmail.com>
2020-12-12 12:19:50 +0800

    complement 1_43

--- ch1/1_43.scm
+++ ch1/1_43.scm
@@ -1,9 +1,18 @@
 (load "./ch1/1_42.scm")
 
+; recursive process
+; (define (repeated f n)
+;   (if (= n 1)
+;       (lambda (x) (f x))
+;       (compose f (repeated f (- n 1)))))
+; ((repeated square 2) 5)
+
+; 等同于
 (define (repeated f n)
   (if (= n 1)
       (lambda (x) (f x))
-      (compose f (repeated f (- n 1)))))
+      (lambda (x) 
+               (f ((repeated f (- n 1)) x)))))
 ((repeated square 2) 5)
 
 ; iterative process todo? 怎么实现?
