12dcab72de9333649764670ae823d8e132d5ffcd
mojiajun <mojiajunmail@gmail.com>
2021-02-08 19:59:29 +0800

    add 2_09

---
+++ ch2/2_09.scm
@@ -0,0 +1,14 @@
+[aL, aH] + [bL, bH] = [aL + bL, aH + bH].
+width = 1/2 * ((aH + bH) - (aL + bL))
+        = 1/2 * ((aH - aL) + (bH - bL))
+        = width of interval a + width of interval b
+
+[aL, aH] - [bL, bH] = [aL - bH, aH - bL].
+区间宽度 = 1/2 * ((aH - bL) - (aL - bH)) = 1/2 * (aH - bL - aL + bH)
+= 1/2 * (aH - aL + bH - bL)  ok
+
+1/2 * (aH - aL)     1/2 * (bH - bL)
+
+所以和与差都是
+
+乘除见http://community.schemewiki.org/?sicp-ex-2.9
