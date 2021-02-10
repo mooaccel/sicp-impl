4c09ae85822de084c319eaece2bcef9011b4df32
mojiajun <mojiajunmail@gmail.com>
2021-02-10 15:41:31 +0800

    add p169_permutation.scm

---
+++ ch2/p169_permutation.scm
@@ -0,0 +1,19 @@
+(define (accumulate op initial sequence) 
+  (if (null? sequence) 
+    initial 
+    (op (car sequence) 
+        (accumulate op initial (cdr sequence)))))
+
+(define (flatmap proc seq) 
+  (accumulate append '() (map proc seq)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item))) sequence))
+
+(define (permutations s)
+  (if (null? s)
+      (list '())
+      (flatmap (lambda (x) (map (lambda (p) (cons x p))
+                                (permutations (remove x s))))
+               s)))
+(permutations (list 1 2 3))
