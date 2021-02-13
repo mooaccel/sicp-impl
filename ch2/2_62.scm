dd1ae31623169edd6c368f3a3f7cb87637219579
mojiajun <mojiajunmail@gmail.com>
2021-02-13 21:44:19 +0800

    modify 2_62.scm

--- ch2/2_62.scm
+++ ch2/2_62.scm
@@ -1,11 +1,23 @@
 (define (union-set set1 set2)
-    (let ((x1 (car set1)) 
-          (x2 (car set2)))
-       (cond ((= x1 x2)
-               (cons x1 (union-set set1 set2)))
-             ((< x1 x2)
-               (cons x1
-                     (union-set (cdr set1) 
-                                set2)))
-             (else (cons x2 (union-set set1 
-                                       (cdr set2)))))))
+    (cond ((and (null? set1) (null? set2))
+            '())
+          ((null? set1)
+            set2)
+          ((null? set2)
+            set1)
+          (else 
+              (let ((x1 (car set1)) 
+                    (x2 (car set2)))
+                      (cond ((= x1 x2)
+                              (cons x1 (union-set (cdr set1) 
+                                                  (cdr set2))))
+                            ((< x1 x2)
+                              (cons x1
+                                    (union-set (cdr set1) 
+                                               set2)))
+                            (else (cons x2 (union-set set1 
+                                                      (cdr set2)))))))
+          ))
+(union-set '() (list 1 2 3))
+(union-set (list 1 2 3) (list 1 3 5))
+(union-set (list 1 2 3) (list 1 3 5 7 9))
