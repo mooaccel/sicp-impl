d2d2d9d3935189efdb75b5fea7ef81bb1e5a9107
mojiajun <mojiajunmail@gmail.com>
2021-02-16 23:28:43 +0800

    finish 2_73 b

--- ch2/2_73/2_73_b/2_73_b_product.scm
+++ ch2/2_73/2_73_b/2_73_b_product.scm
@@ -17,34 +17,35 @@
               ((and (number? x) (number? y))
                 (* x y))
               (else
-                (attach-tag 'product x y))))
+                (attach-tag '* x y))))
   (define (multiplier-impl p) (car p))
   (define (multiplicand-impl p) (cadr p))
   ; exp没有tag
   (define (deriv-impl exp var) 
     ; deriv需要重新dispatch, 所以是deriv
+    ; make-sum, make-product通过dispatch或者直接调用-impl都行, 只不过-impl版本少一次查表过程
     (make-sum (make-product (multiplier-impl exp) 
                             (deriv (multiplicand-impl exp) var)) 
               (make-product (deriv (multiplier-impl exp) var) 
                             (multiplicand-impl exp))))
   ; interface to the rest of the system
   ;(define (tag x) (attach-tag ' x)) 
-  (put 'make-product 'product make-product-impl)
-  (put 'multiplier 'product multiplier-impl)
-  (put 'multiplicand 'product multiplicand-impl)
-  (put 'deriv 'product deriv-impl) 
+  (put 'make-product '* make-product-impl)
+  (put 'multiplier '* multiplier-impl)
+  (put 'multiplicand '* multiplicand-impl)
+  (put 'deriv '* deriv-impl) 
   'done)
 
 ; 给其他模块用
 (define (make-product x y)
-  ((get 'make-product 'product) x y)
+  ((get 'make-product '*) x y)
 )
 ; 为什么要加上(contents s)
 ; multiplier是在外部调用的API函数, 输入参数s是带tag的
 (define (multiplier s)
   ; 而dispatch之后, 内层的multiplier-impl是不带tag的
-  ((get 'multiplier 'product) (contents s))
+  ((get 'multiplier '*) (contents s))
 )
 (define (multiplicand s)
-  ((get 'multiplicand 'product) (contents s))
+  ((get 'multiplicand '*) (contents s))
 )
--- ch2/2_73/2_73_b/2_73_b_test.scm
+++ ch2/2_73/2_73_b/2_73_b_test.scm
@@ -19,3 +19,9 @@ sum-1
 (deriv (make-sum 'x (make-product 'x (make-product 'x 'x))) 'x)
 ;((get 'deriv 'sum) (contents sum-1) 'x)
 ;(list 'sum 'x '3) 'x)
+
+; 因为符号改成了'+ '*
+; 而在deriv实现里, '+ '*既是操作符, 又是tag
+; 所以可以下面这么写
+; (operator exp)拿到操作符, 也即tag, 这是这个例子比较妙的一点, tag同时也是操作符
+(deriv '(+ (* 10 x) x) 'x)
