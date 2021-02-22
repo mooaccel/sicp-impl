2c236e5b56a91de0af1d736308884fbc7f08c1a3
mojiajun <mojiajunmail@gmail.com>
2021-02-22 11:44:21 +0800

    add 3_28

---
+++ ch3/3_28.scm
@@ -0,0 +1,15 @@
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure) 
+    (let ((new-value (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda () (set-signal! output new-value))))) 
+
+  (add-action! a1 or-action-procedure) 
+  (add-action! a2 or-action-procedure) 
+  'ok)
+(define (logical-or wire1 wire2)
+  (cond ((and (= wire1 0)
+              (= wire2 0)) 0)
+        ((or (= wire1 1)
+             (= wire2 1)) 1)
+        (else (error "Invalid signal" wire1 wire2))))
---
+++ ch3/p374_circuit/p374_circuit.scm
@@ -0,0 +1,28 @@
+(define (inverter input output)
+  (define (invert-input) 
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay 
+                   (lambda () (set-signal! output new-value)))))
+
+  (add-action! input invert-input) 
+  'ok)
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure) 
+    (let ((new-value (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda () (set-signal! output new-value))))) 
+
+  (add-action! a1 and-action-procedure) 
+  (add-action! a2 and-action-procedure) 
+  'ok)
+(define (logical-and wire1 wire2)
+  (cond ((and (= wire1 1)
+              (= wire2 1)) 1)
+        ((or (= wire1 0)
+             (= wire2 0)) 0)
+        (else (error "Invalid signal" wire1 wire2))))
