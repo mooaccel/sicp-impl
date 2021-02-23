23b324b50ddd9ccf39f272d11853293f126c1ee0
mojiajun <mojiajunmail@gmail.com>
2021-02-23 21:23:23 +0800

    add ch3.3.5 constraint system

---
+++ ch3/p388_constraint_system/all_constraint_system.scm
@@ -0,0 +1,12 @@
+; inform-about-value, inform-about-no-value
+(load "./ch3/p388_constraint_system/p393_constraint_interface.scm")
+; adder
+(load "./ch3/p388_constraint_system/p392_adder.scm")
+; multiplier
+(load "./ch3/p388_constraint_system/p393_multiplier.scm")
+; constant 
+(load "./ch3/p388_constraint_system/p395_constant.scm")
+; probe
+(load "./ch3/p388_constraint_system/p395_probe.scm")
+; make-connector
+(load "./ch3/p388_constraint_system/p396_connector.scm")
