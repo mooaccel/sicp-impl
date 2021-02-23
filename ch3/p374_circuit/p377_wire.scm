2c8b13ceb6a5b8b8c542567218f6a34f7904f8fe
mojiajun <mojiajunmail@gmail.com>
2021-02-23 13:40:57 +0800

    add wire some comment

--- ch3/p374_circuit/p377_wire.scm
+++ ch3/p374_circuit/p377_wire.scm
@@ -1,6 +1,11 @@
 (define (make-wire)
   (let ((signal-value 0) 
-        (action-procedures '())) 
+        (action-procedures '()))     ; 这里其实是last in, first out性质, 但是无所谓, 例如Figure 3.25 half adder
+                                     ; a的改变被or-gate, and-gate都盯着
+                                     ; 所以a上面有两个回调action
+                                     ; 在实际电子电路里, 这里是同时到达的, 但是这个所谓的同时, 可能在时间测度很小的时候, 也可能存在先后关系
+                                     ; 所以a的改变先通知Figure 3.25的or-gate还是and-gate没啥影响, 我们也不应该依赖于这种特定顺序
+                                     ; 所以在实现ch3.3.4电路模拟器的时候, 这里就无所谓了, 随便先传播给谁
 
     (define (set-my-signal! new-value) 
       (if (not (= signal-value new-value)) 
