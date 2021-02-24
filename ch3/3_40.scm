6f326f654e9c38ce6f208569e6c8f6772d511a3f
mojiajun <mojiajunmail@gmail.com>
2021-02-24 20:45:47 +0800

    add 3_40

---
+++ ch3/3_40.scm
@@ -0,0 +1,13 @@
+; http://community.schemewiki.org/?sicp-ex-3.40
+; 差不多是这么想的, 分成3个阶段, 然后读又需要读多次(这里可以改成只读一次)
+; P1, abcd
+; P2, vwxyz
+; 想象一个人左右四个手指头, 右手五个, 然后分别岔开的去形成新的可能性
+; 总的可能性应该有数学公式能计算出来
+
+Unique results:
+[100, 1000, 10000, 100000, 1000000]
+
+After serializing P1 and P2:
+Only event sequence 1 and 2 remain:
+[1000000]
