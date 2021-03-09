8b97a2e91140ae9c36100786918e8824c2165806
mojiajun <mojiajunmail@gmail.com>
2021-03-09 13:36:53 +0800

    add 3_66 todo, 3_67 ok

---
+++ ch3/3_67.scm
@@ -0,0 +1,18 @@
+; integers
+(define ones (cons-stream 1 ones))
+(define (add-streams s1 s2) (stream-map + s1 s2))
+(define integers (cons-stream 1 (add-streams ones integers)))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1) 
+      s2 
+      (cons-stream (stream-car s1) 
+                   (interleave s2 (stream-cdr s1)))))
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (pairs (stream-cdr s) t))))
+(stream-head (pairs integers integers) 20)
